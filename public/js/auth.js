558b4a6935d5f9bcf47b244d743fa8401c1ec9ce
pomfy113 <pomfy113@gmail.com>
2018-01-18 21:00:57 -0800

    Display error, basic log-in testing

---
+++ public/js/auth.js
@@ -0,0 +1,26 @@
+$('#register').submit(function (e) {
+  let timerFunc;
+
+  e.preventDefault();
+  let username = $('#username').val();
+  let password = $('#password').val();
+
+  let data = {
+      "username": username,
+      "password": password
+  }
+
+  $.post('/register', data, function(data){
+      window.location.replace("/");
+  }).catch((err) => {
+      console.log(err.responseText)
+      clearTimeout(timerFunc)
+      $('#errorbox').html(err.responseText)
+      $('#errorbox').show()
+      timerFunc = window.setTimeout(hidewindow, 1000)
+  })
+
+  const hidewindow = function(){
+      $('#errorbox').fadeOut(400)
+  }
+});
